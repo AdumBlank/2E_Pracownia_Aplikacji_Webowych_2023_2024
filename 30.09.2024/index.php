626123d59dd0c1a9d86c2bc6c4ec96e750953175
Adam Blank <89538276+AdumBlank@users.noreply.github.com>
2024-09-30 16:45:43 +0200

    Add files via upload

---
+++ 30.09.2024/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+
+</head>
+<body>
+    <?php 
+//zad 1
+//$c = 12;
+//$f = $c *9/5+32;
+
+//echo("Stopnie Fahrenheita:" . $f);
+
+//zad 2
+/*
+$a = 123;
+$b = 321;
+
+$wynikmno = "*"; 
+$wynikdzi = "/";
+$wynikdod = "+";
+$wynikode = "-";
+echo("$a $wynikode $b =". $a - $b);
+echo("$a $wynikdod $b =". $a + $b);
+echo("$a $wynikmno $b =". $a * $b);
+echo("$a $wynikdzi $b =". $a / $b);
+*/
+
+//zad 3
+/*
+$r = 12;
+$pi = 3.14159;
+$pole = 2*$pi*$r;
+$pole = round($pole,3);
+echo("Pole kola: $pole");
+*/
+
+//zad 4
+$wiek = "20";
+$wiek = (integer)$wiek;
+echo($wiek * 365);
+
+?>
+</body>
+</html>
